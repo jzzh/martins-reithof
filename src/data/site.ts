ab225f016ab2c8d76c5b74f589be2c373bad629b
Jochen Zehe <jochen.zehe@gmail.com>
2026-09-14 16:00:02 +0200

    Update email address

--- src/data/site.ts
+++ src/data/site.ts
@@ -12,7 +12,7 @@ export const site = {
   },
   phoneStable: { label: "077 448 19 56", href: "tel:+41774481956", display: "077 448 19 56" },
   phoneMartin: { label: "076 606 76 21", href: "tel:+41766067621" },
-  email: { label: "info@martins-reithof.ch", href: "mailto:info@martins-reithof.ch" },
+  email: { label: "martinkopecky86@gmail.com", href: "mailto:martinkopecky86@gmail.com" },
   instagram: { label: "Martin's Reithof", href: "https://www.instagram.com/martinsreithof/" },
 };
 
